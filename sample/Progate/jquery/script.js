8fb40ec535a33af1d45a3fdfb57a2d8b8a47d5b4
aki <aki.sugeryp@gmail.com>
2020-03-28 22:17:56 +0900

    add sample es6 jquery

---
+++ sample/Progate/jquery/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
+  $("#language-wrapper").hover(
+    function(){
+      $(".language-text").fadeIn();
+    },
+    function(){
+      $(".language-text").fadeOut();
+    });
+});
