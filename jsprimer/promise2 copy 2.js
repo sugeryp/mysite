5ad2d95ad79cfec8cdcc9ea9e8c9fad8a6b87533
sugeryp <aki.sugeryp@gmail.com>
2020-07-16 14:45:54 +0900

    up

---
+++ jsprimer/promise2 copy 2.js
@@ -0,0 +1,21 @@
+/**
+ * 1000ミリ秒未満のランダムなタイミングでレスポンスを疑似的にデータ取得する関数
+ * 指定した`path`にデータがある場合、成功として**Resolved**状態のPromiseオブジェクトを返す
+ * 指定した`path`にデータがない場合、失敗として**Rejected**状態のPromiseオブジェクトを返す
+ */
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+// `then`メソッドで成功時と失敗時に呼ばれるコールバック関数を登録
+// /success/data のリソースは存在するので成功しonFulfilledが呼ばれる
+
+console.log("before");
+dummyFetch("/success/data").then(function onFulfilled(response) {
+    console.log(response, "ton!"); // => { body: "Response body of /success/data" }
+}, function onRejected(error) {
+    // この行は実行されません
+});
+console.log("midle");
+// /failure/data のリソースは存在しないのでonRejectedが呼ばれる
---
+++ jsprimer/promise2 copy.js
@@ -0,0 +1,25 @@
+/**
+ * 1000ミリ秒未満のランダムなタイミングでレスポンスを疑似的にデータ取得する関数
+ * 指定した`path`にデータがある場合、成功として**Resolved**状態のPromiseオブジェクトを返す
+ * 指定した`path`にデータがない場合、失敗として**Rejected**状態のPromiseオブジェクトを返す
+ */
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+        if (path.startsWith("/success")) {
+            resolve({ body: `Response body of ${path}` });
+        } else {
+            reject(new Error("NOT FOUND"));
+        };
+    });
+};
+// `then`メソッドで成功時と失敗時に呼ばれるコールバック関数を登録
+// /success/data のリソースは存在するので成功しonFulfilledが呼ばれる
+
+console.log("before");
+dummyFetch("/success/data").then(function onFulfilled(response) {
+    console.log(response); // => { body: "Response body of /success/data" }
+}, function onRejected(error) {
+    // この行は実行されません
+});
+console.log("midle");
+// /failure/data のリソースは存在しないのでonRejectedが呼ばれる
